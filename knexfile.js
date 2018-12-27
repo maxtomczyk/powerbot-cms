3e888d54aed7673ca6daf7eff3d34dd7b6aa19ac
Maksymimlian Tomczyk <m.tomczyk.dev@gmail.com>
2018-12-27 23:54:46 +0100

    Added knexfile for futher migrations

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+const config = require('./config/config')
+
+module.exports = {
+  client: 'pg',
+  useNullAsDefault: true,
+  pool: { min: 0, max: 50 },
+  connection: process.env.DATABASE_URL || {
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.name
+  }
+}
