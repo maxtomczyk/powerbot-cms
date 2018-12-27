352cb415f4b9d2cc43a3e2a5b0a9704087f94c73
Maksymimlian Tomczyk <m.tomczyk.dev@gmail.com>
2018-12-27 14:31:27 +0100

    Adde try/catch in attachments.js

--- modules/attachments.js
+++ modules/attachments.js
@@ -4,19 +4,23 @@ const logger = require('./logger.js')
 const config = require('../config/config.js')
 
 async function get (id) {
-  let aId = await redis.getAsync(`attachment-id:${id}`)
-  logger.debug(`Attachment '${id}' found in cache memory!`)
+  try {
+    let aId = await redis.getAsync(`attachment-id:${id}`)
+    logger.debug(`Attachment '${id}' found in cache memory!`)
 
-  if (aId) return aId
+    if (aId) return aId
 
-  let row = await knex('attachments').where('name', id).first()
-  if (!row) return null
-  logger.debug(`Attachment '${id}' fetched from database`)
-  aId = row.attachment_id
-  logger.debug(`Saving attachment '${id}' to cache memory`)
-  redis.set(`attachment-id:${id}`, aId, 'EX', config.redis.timeouts.attachments)
+    let row = await knex('attachments').where('name', id).first()
+    if (!row) return null
+    logger.debug(`Attachment '${id}' fetched from database`)
+    aId = row.attachment_id
+    logger.debug(`Saving attachment '${id}' to cache memory`)
+    redis.set(`attachment-id:${id}`, aId, 'EX', config.redis.timeouts.attachments)
 
-  return aId
+    return aId
+  } catch (e) {
+      throw e
+  }
 }
 
 module.exports = {
