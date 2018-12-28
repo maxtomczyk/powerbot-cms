44bc8243af5cd19708b60cc016bfb833ef7044fc
Maksymimlian Tomczyk <m.tomczyk.dev@gmail.com>
2018-12-28 02:03:34 +0100

    Created migration file

---
+++ migrations/20181228011922_users_data_table.js
@@ -0,0 +1,14 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('users_data', function (t) {
+    t.increments()
+    t.string('name', 32).notNullable()
+    t.json('data')
+    t.integer('user_id').notNullable().references('users.id').onUpdate('cascade').onDelete('cascade')
+
+    t.unique('id')
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('users_data')
+}
