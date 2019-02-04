bcfc5f64a8d11d75a6028a1be549ecd46b0e2ed8
Maksymilian Tomczyk <tomczyk.maksymilian69@gmail.com>
2019-02-04 01:57:35 +0100

    Changed project name in api template file

--- modules/custom_routes.template.js
+++ modules/custom_routes.template.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
-const auth = require('../node_modules/incredbot-cms/modules/auth.js')()
-const logger = require('../node_modules/incredbot-cms/modules/logger.js')
+const auth = require('../node_modules/powerbot-cms/modules/auth.js')()
+const logger = require('../node_modules/powerbot-cms/modules/logger.js')
 const api = require('./api')
 
 router.get('/test', auth.authenticate(), async (req, res) => {
