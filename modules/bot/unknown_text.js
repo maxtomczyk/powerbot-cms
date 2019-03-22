f0c2b67c49c4598e4c7bcac1bea1c6483f4cdeb5
Maksymilian Tomczyk <max@MacBook-Pro-Maksymilian.local>
2019-03-22 19:06:24 +0100

    Fixed error caused by no default dialogflow intent

--- modules/bot/unknown_text.js
+++ modules/bot/unknown_text.js
@@ -24,15 +24,17 @@ function logDialogflowDebug (nlpData, message) {
   console.log()
   logger.debug(`<| DIALOGFLOW DEBUG DATA |>`)
   logger.debug(`query: ${message.text}`)
-  logger.debug(`intent: ${nlpData.intent.displayName}`)
+  logger.debug(`intent: ${(nlpData.intent) ? nlpData.intent.displayName : null}`)
   logger.debug(`confidence: ${nlpData.intentDetectionConfidence}`)
   logger.debug(`completed: ${nlpData.allRequiredParamsPresent}`)
-  logger.debug(`suggested response: ${nlpData.fulfillmentText}`)
-  if (nlpData.parameters.fields && Object.keys(nlpData.parameters.fields).length) logger.debug(`parameters:`)
-  for (const paramName in nlpData.parameters.fields) {
-    if (nlpData.parameters.fields[paramName].kind === 'stringValue') logger.debug(`   ${paramName}: ${nlpData.parameters.fields[paramName].stringValue}`)
-    else if (nlpData.parameters.fields[paramName].kind === 'numberValue') logger.debug(`   ${paramName}: ${nlpData.parameters.fields[paramName].numberValue}`)
-    else logger.debug(`  ${paramName}: ${JSON.stringify(nlpData.parameters.fields[paramName]) || null}`)
+  logger.debug(`suggested response: ${nlpData.fulfillmentText || null}`)
+  if (nlpData.parameters && nlpData.parameters.fields && Object.keys(nlpData.parameters.fields).length) {
+    logger.debug(`parameters:`)
+    for (const paramName in nlpData.parameters.fields) {
+      if (nlpData.parameters.fields[paramName].kind === 'stringValue') logger.debug(`   ${paramName}: ${nlpData.parameters.fields[paramName].stringValue}`)
+      else if (nlpData.parameters.fields[paramName].kind === 'numberValue') logger.debug(`   ${paramName}: ${nlpData.parameters.fields[paramName].numberValue}`)
+      else logger.debug(`  ${paramName}: ${JSON.stringify(nlpData.parameters.fields[paramName]) || null}`)
+    }
   }
   console.log()
   console.log('------------------------------------------------------------')
@@ -42,35 +44,36 @@ function simplifyDialogflowResponse (nlpData) {
   if (!nlpData) return
   let simple = {}
   simple.query = nlpData.queryText
-  simple.intent = nlpData.intent.displayName
+  simple.intent = (nlpData.intent) ? nlpData.intent.displayName : null
   simple.intentConfidence = nlpData.intentDetectionConfidence
   simple.completeParams = nlpData.allRequiredParamsPresent
-  simple.isFallback = nlpData.intent.isFallback
-  simple.reset = nlpData.intent.resetContexts
+  simple.isFallback = (nlpData.intent) ? nlpData.intent.isFallback : null
+  simple.reset = (nlpData.intent) ? nlpData.intent.resetContexts : null
   simple.suggestedResponse = nlpData.fulfillmentText
   simple.parameters = {}
-  for (const paramName in nlpData.parameters.fields) {
-    if (nlpData.parameters.fields[paramName].kind === 'stringValue') simple.parameters[paramName] = nlpData.parameters.fields[paramName].stringValue
-    else if (nlpData.parameters.fields[paramName].kind === 'numberValue') simple.parameters[paramName] = nlpData.parameters.fields[paramName].numberValue
-    else {
-      simple.parameters[paramName] = nlpData.parameters.fields[paramName]
-      simple.parameters.raw = true
+
+  if (nlpData.parameters && nlpData.parameters.fields) {
+    for (const paramName in nlpData.parameters.fields) {
+      if (nlpData.parameters.fields[paramName].kind === 'stringValue') simple.parameters[paramName] = nlpData.parameters.fields[paramName].stringValue
+      else if (nlpData.parameters.fields[paramName].kind === 'numberValue') simple.parameters[paramName] = nlpData.parameters.fields[paramName].numberValue
+      else {
+        simple.parameters[paramName] = nlpData.parameters.fields[paramName]
+        simple.parameters.raw = true
+      }
     }
   }
+
   return simple
 }
 
 async function useDialogflow (message, user) {
   try {
     if (!config.dialogflow.enable) return false
-
     let sessionId = await redis.getAsync(`dialogflow-session-user:${user.id}`)
     if (!sessionId) sessionId = uuid.v4()
     redis.set(`dialogflow-session-user:${user.id}`, sessionId, 'EX', config.dialogflow.sessionTimeout)
-
     const sessionClient = new dialogflow.SessionsClient()
     const sessionPath = sessionClient.sessionPath(config.dialogflow.projectId, sessionId)
-
     const request = {
       session: sessionPath,
       queryInput: {
@@ -92,7 +95,6 @@ module.exports = async function (message, user) {
   try {
     const patterns = await preMessages.loadRegexMessages()
     let success = false
-
     for (const pattern of patterns) {
       const regex = new RegExp(pattern.regex_body, pattern.regex_flags)
       if (regex.test(message.text)) {
